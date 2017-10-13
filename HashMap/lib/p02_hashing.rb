19bb7066d4bee79bb57af83677fb78d876ac6448
Jeffrey Ren <jeffyiren@gmail.com>
2017-10-13 15:06:55 -0700

    create p01 thru p04

--- HashMap/lib/p02_hashing.rb
+++ HashMap/lib/p02_hashing.rb
@@ -1,15 +1,22 @@
+require 'byebug'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
-    self.inject(:^)
+    self.each_with_index.inject(0) do |acc, (el, i)|
+      (el.hash + i.hash) ^ acc
+    end
   end
 end
 
 class String
+  ALPHA = ("a".."z").to_a
   def hash
+    ans = self.chars.map { |el| ALPHA.index(el) }
+    ans.hash
   end
 end
 
@@ -17,6 +24,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.each.inject(0) do |acc, key|
+      (key.hash + self[key].hash) ^ acc
+    end
+
+    
   end
 end
